0f1919e6f2384525b8c3431030faf3c27078e0f1
Diegolo2 <albad2346@gmail.com>
2025-10-21 16:03:23 -0400

    Agregar archivos de proyecto.

---
+++ Mover un personaje 3D/Mover un personaje 3D Metodo 1.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+public class Moverunpersonaje : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+    void Update()
+    {
+        Vector3 movementInput = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            movementInput.z = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            movementInput.z = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            movementInput.x = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            movementInput.x = -1;
+
+        }
+
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            movementInput.z = 1;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            movementInput.z = -1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            movementInput.x = 1;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            movementInput.x = -1;
+
+        }
+
+        Move(movementInput);
+
+        void Move(Vector3 direction)
+
+        {
+            transform.position += direction.normalized * speed * Time.deltaTime;
+
+        }
+    }
+}
