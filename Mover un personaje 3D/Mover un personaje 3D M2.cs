fced0795f3d0417d9719b3e1b77d7f2d8b2e866f
Diegolo2 <albad2346@gmail.com>
2025-10-21 18:23:54 -0400

    Mover un personaje M2 fisicas & M3 CC Controller

---
+++ Mover un personaje 3D/Mover un personaje 3D M2.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+public class MoverunpersonajeedM2 : MonoBehaviour
+{
+    public float speed = 5f;
+    Rigidbody rb;
+    Vector3 movementInput;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    public void Update()
+    {
+        Vector3 movementInput = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            movementInput.z = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            movementInput.z = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            movementInput.x = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            movementInput.x = -1;
+
+        }
+
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            movementInput.z = 1;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            movementInput.z = -1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            movementInput.x = 1;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            movementInput.x = -1;
+
+        }
+    }
+
+    protected void FixedUpdate()
+
+    {
+        Move(movementInput);
+    }
+
+
+    void Move(Vector3 direction)
+
+    {
+        rb.AddForce(direction.normalized * speed, ForceMode.Acceleration);
+        //rb.MovePosition (rb.position + direction.normalized * speed * Time.fixedDeltaTime);
+
+    }
+
+}
+
+void Move(Vector3 direction)
+
+    {
+        controller.SimpleMove(direction.normalized * speed);
+
+
+    }
+
+}
---
+++ Mover un personaje 3D/Mover un personaje 3D M3 Character Controller.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+public class moverunpersonaje3dm3 : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+    CharacterController controller;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    public void Update()
+    {
+        Vector3 movementInput = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            movementInput.z = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            movementInput.z = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            movementInput.x = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            movementInput.x = -1;
+
+        }
+
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            movementInput.z = 1;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            movementInput.z = -1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            movementInput.x = 1;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            movementInput.x = -1;
+
+        }
+
+        Move(movementInput);
+    }
+
+
+    void Move(Vector3 direction)
+
+    {
+        controller.SimpleMove(direction.normalized * speed);
+
+
+    }
+
+}
